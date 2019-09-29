fcfbdc10364b8a3d44f2a9c07f9b07631fa7a36a
ericwindmill <ewindmill@greenbits.com>
2019-09-29 12:11:39 -0700

    adds favicon, finishes all image uploads

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,1 @@
-import 'dart:html';
-
-main() {
-  var message = ParagraphElement()
-    ..text = "This paragraph was added using Dart";
-  var host = querySelector('#content');
-  host.children.add(message);
-}
+main() {}
